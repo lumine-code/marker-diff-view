4612687a2f7b787a0e3c6f9add7adc9fc19bc939
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-07-25 16:02:21 +0200

    Track layers through the scrollmap service contract instead of editor internals

--- spec/scrollmap-diff-view-spec.js
+++ spec/scrollmap-diff-view-spec.js
@@ -20,10 +20,6 @@ describe("scrollmap-diff-view", () => {
     });
     fake.updateSync = fake.update;
     fake.refresh = () => {};
-    targetEditor.scrollmap = {
-      layers: new Map([[provider.name, fake]]),
-      updateView() {},
-    };
     if (provider.initialize) {
       provider.initialize(fake);
     }
